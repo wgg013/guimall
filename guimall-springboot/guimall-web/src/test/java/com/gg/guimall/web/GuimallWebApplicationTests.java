b694b0f36265f0abcf47d1960815c27c38bfcfe0
wgg <wlyuan0017@163.com>
2025-12-20 12:42:58 +0800

    SpringBoot后端工程搭建
    搭建SpringBoot多模块工程

---
+++ guimall-springboot/guimall-web/src/test/java/com/gg/guimall/web/GuimallWebApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gg.guimall.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class GuimallWebApplicationTests {
+    @Test
+    public void test(){
+        //编写单元测试
+    }
+}
